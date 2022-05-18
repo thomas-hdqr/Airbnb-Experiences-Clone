422e52437337bfb2e2a2e1862a74078a614f182e
Thomas <thomashaudiquer@gmail.com>
2022-05-18 09:50:17 +0100

    first setup for the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+        <nav className="navbar-logo">
+            <img className="navbar-logo" src="./images/airbnb-logo.png" alt="airbnb logo" />
+        </nav>
+    )
+}
+
+export default Navbar;
